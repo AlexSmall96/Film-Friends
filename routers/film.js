fbdc07b7ebc88e123ee1d267f1c7aecaebb3bb76
Alex Small <alex.small739@gmail.com>
2024-08-19 13:45:28 +0100

    Add view film router

--- routers/film.js
+++ routers/film.js
@@ -13,4 +13,16 @@ router.post('/films', async (req, res) => {
     }
 })
 
+// View film data for one film (get multiple films is handled by router.get('/users/:id, ...) in routers/user.js)
+router.get('/films/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const film = await Film.findById(_id)
+        res.status(200).send({ film })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
 module.exports = router
--- routers/film.test.js
+++ routers/film.test.js
@@ -4,21 +4,13 @@ const mongoose = require('mongoose')
 const User = require('../models/user')
 const Film = require('../models/film')
 
-// Wipe database before each test is run and setup initial user
+// Define test data
 const userTwoId = new mongoose.Types.ObjectId()
-const userTwo = {
-    _id: userTwoId,
-    username: 'Jon',
-    email: 'jon@example.com',
-    password: '56what!!',
-}
+const filmTwoId = new mongoose.Types.ObjectId()
+const userTwo = {_id: userTwoId, username: 'Jon', email: 'jon@example.com', password: '56what!!'}
+const filmTwo = {_id: filmTwoId, title: 'film two', imdbID: 'g123', owner: userTwoId}
 
-const filmTwo = {
-    title: 'film two',
-    imdbID: 'g123',
-    owner: userTwoId
-}
-
+// Wipe database before each test and setup test data
 beforeEach(async () => {
     await User.deleteMany()
     await Film.deleteMany()
@@ -27,20 +19,11 @@ beforeEach(async () => {
 })
 
 // Close database connection after tests have run
-afterAll(async () => {
-    mongoose.connection.close()
-})
+afterAll(async () => {mongoose.connection.close()})
 
 test('Should create a new film', async () => {
     // Correct status code
-    const response = await request(app)
-    .post('/films')
-    .send({
-        title: 'A great film',
-        imdbID: 'a123',
-        owner: userTwoId
-    })
-    .expect(201)
+    const response = await request(app).post('/films').send({title: 'A great film', imdbID: 'a123', owner: userTwoId}).expect(201)
     // Assert that the database was changed correctly
     const film = Film.findById(response.body.id)
     expect(film).not.toBeNull()
@@ -54,38 +37,19 @@ test('Should create a new film', async () => {
 
 test('Film creation should fail with invalid data', async () => {
     // imdbID already taken
-    await request(app)
-    .post('/films')
-    .send({
-        title: 'film three',
-        imdbID: 'g123',
-        owner: userTwoId
-    })
-    .expect(400)
+    await request(app).post('/films').send({title: 'film three', imdbID: 'g123', owner: userTwoId}).expect(400)
     // Missing title
-    await request(app)
-    .post('/films')
-    .send({
-        imdbID: 'e123',
-        owner: userTwoId
-    })
-    .expect(400)
+    await request(app).post('/films').send({imdbID: 'e123', owner: userTwoId}).expect(400)
     // Missing owner
-    await request(app)
-    .post('/films')
-    .send({
-        title: 'film three',
-        imdbID: 'f123',
-    })
-    .expect(400)
+    await request(app).post('/films').send({title: 'film three', imdbID: 'f123'}).expect(400)
     // Invalid rating
-    await request(app)
-    .post('/films')
-    .send({
-        title: 'film three',
-        imdbID: 'h123',
-        owner: userTwoId,
-        userRating: 6
-    })
-    .expect(400)
+    await request(app).post('/films').send({title: 'film three', imdbID: 'h123', owner: userTwoId, userRating: 6}).expect(400)
+})
+
+test('Should view data for a single film', async () => {
+    // Correct status code
+    const response = await request(app).get(`/films/${filmTwo._id}`).expect(200)
+    // Correct data is recieved
+    expect(response.body.film.title).toBe('film two')
+    expect(response.body.film.imdbID).toBe('g123')
 })
