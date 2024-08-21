0bef961122829dfb276266e6c274021f2fc4da08
Alex Small <alex.small739@gmail.com>
2024-08-21 16:38:25 +0100

    Add authentication to film routes

--- routers/film.js
+++ routers/film.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = new express.Router()
 const Film = require('../models/film')
+const auth = require('../middleware/auth')
 
 // Create a film (save film to watchlist)
-router.post('/films', async (req, res) => {
+router.post('/films', auth, async (req, res) => {
     const film = new Film(req.body)
     try {
         await film.save()
@@ -15,7 +16,7 @@ router.post('/films', async (req, res) => {
 
 // View film data for one film (get multiple films is handled by router.get('/users/:id, ...) in routers/user.js)
 // Need to add to authentication to ensure user is owner of film
-router.get('/films/:id', async (req, res) => {
+router.get('/films/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
         const film = await Film.findById(_id)
@@ -26,7 +27,7 @@ router.get('/films/:id', async (req, res) => {
 })
 
 // Update film data
-router.patch('/films/:id', async (req, res) => {
+router.patch('/films/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
         const film = await Film.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
@@ -40,7 +41,7 @@ router.patch('/films/:id', async (req, res) => {
 })
 
 // Delete film
-router.delete('/films/:id', async (req, res) => {
+router.delete('/films/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
         const film = await Film.findOneAndDelete({_id:_id})
--- routers/film.test.js
+++ routers/film.test.js
@@ -3,19 +3,30 @@ const app = require('../setupApp')
 const mongoose = require('mongoose')
 const User = require('../models/user')
 const Film = require('../models/film')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
 
 // Define test data
-const userTwoId = new mongoose.Types.ObjectId()
-const filmTwoId = new mongoose.Types.ObjectId()
-const userTwo = {_id: userTwoId, username: 'Jon', email: 'jon@example.com', password: '56what!!'}
-const filmTwo = {_id: filmTwoId, title: 'film two', imdbID: 'g123', owner: userTwoId}
+const userThreeId = new mongoose.Types.ObjectId()
+const filmThreeId = new mongoose.Types.ObjectId()
+const userThree = {
+    _id: userThreeId, 
+    username: 'Jon', 
+    email: 'jon@example.com', 
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({_id: userThreeId}, JWT_SECRET)
+    }]
+}
+const userThreeAuth = ['Authorization', `Bearer ${userThree.tokens[0].token}`]
+const filmThree = {_id: filmThreeId, title: 'film two', imdbID: 'g123', owner: userThreeId}
 
 // Wipe database before each test and setup test data
 beforeEach(async () => {
     await User.deleteMany()
     await Film.deleteMany()
-    await new User(userTwo).save()
-    await new Film(filmTwo).save()
+    await new User(userThree).save()
+    await new Film(filmThree).save()
 })
 
 // Close database connection after tests have run
@@ -24,7 +35,10 @@ afterAll(async () => {mongoose.connection.close()})
 // Create a film
 test('Should create a new film', async () => {
     // Correct status code
-    const response = await request(app).post('/films').send({title: 'A great film', imdbID: 'a123', owner: userTwoId}).expect(201)
+    const response = await request(app).post('/films')
+    .send({title: 'A great film', imdbID: 'a123', owner: userThreeId})
+    .set(...userThreeAuth)
+    .expect(201)
     // Assert that the database was changed correctly
     const film = Film.findById(response.body.id)
     expect(film).not.toBeNull()
@@ -35,67 +49,97 @@ test('Should create a new film', async () => {
     expect(response.body.film.watched).toBe(false)
     expect(response.body.film.notes).toBe('')
 })
-
 test('Film creation should fail with invalid data', async () => {
     // imdbID already taken
-    await request(app).post('/films').send({title: 'film three', imdbID: 'g123', owner: userTwoId}).expect(400)
+    await request(app).post('/films').send({title: 'film three', imdbID: 'g123', owner: userThreeId}).set(...userThreeAuth)
+    .expect(400)
     // Missing title
-    await request(app).post('/films').send({imdbID: 'e123', owner: userTwoId}).expect(400)
+    await request(app).post('/films').send({imdbID: 'e123', owner: userThreeId}).set(...userThreeAuth)
+    .expect(400)
     // Missing owner
-    await request(app).post('/films').send({title: 'film three', imdbID: 'f123'}).expect(400)
+    await request(app).post('/films').send({title: 'film three', imdbID: 'f123'}).set(...userThreeAuth)
+    .expect(400)
     // Invalid rating
-    await request(app).post('/films').send({title: 'film three', imdbID: 'h123', owner: userTwoId, userRating: 6}).expect(400)
+    await request(app).post('/films').send({title: 'film three', imdbID: 'h123', owner: userThreeId, userRating: 6}).set(...userThreeAuth)
+    .expect(400)
+})
+test('Film creation should fail when not authenticated', async () => {
+    // Correct status code
+    const response = await request(app).post('/films').send({title: 'A great film', imdbID: 'a123', owner: userThreeId})
+    .expect(401)
+    // Correct error message
+    expect(response.body.error).toBe('Please authenticate.')
 })
 
 // View film
 test('Should view data for a single film', async () => {
     // Correct status code
-    const response = await request(app).get(`/films/${filmTwo._id}`).expect(200)
+    const response = await request(app).get(`/films/${filmThree._id}`).set(...userThreeAuth).expect(200)
     // Correct data is recieved
     expect(response.body.film.title).toBe('film two')
     expect(response.body.film.imdbID).toBe('g123')
 })
-
 test('Get film should fail with invalid id', async () => {
     // Correct status code
-    await request(app).get('/films/123').expect(400)
+    await request(app).get('/films/123').set(...userThreeAuth).expect(400)
+})
+test('Get film should fail when not authenticated', async () => {
+    // Correct status code
+    const response = await request(app).get(`/films/${filmThree._id}`)
+    .expect(401)
+    // Correct error message
+    expect(response.body.error).toBe('Please authenticate.')
 })
 
 // Update film
 test('Should update film with valid data', async () => {
     // Correct status code
-    await request(app).patch(`/films/${filmTwo._id}`).send({
+    await request(app).patch(`/films/${filmThree._id}`).set(...userThreeAuth).send({
         "watched": true,
         "public": true,
         "userRating": 4.5,
         "notes": "a great film, would reccomend"
     }).expect(200)
     // Assert that the database was changed correctly
-    const film = await Film.findById(filmTwo._id)
+    const film = await Film.findById(filmThree._id)
     expect(film.watched).toBe(true)
     expect(film.public).toBe(true)
     expect(film.userRating).toBe(4.5)
     expect(film.notes).toBe("a great film, would reccomend")
 })
-
 test('Film update should fail with invalid data or invalid id', async () => {
     // Invalid id
-    await request(app).patch('/films/123').send({'watched': true}).expect(400)
+    await request(app).patch('/films/123').send({'watched': true}).set(...userThreeAuth).expect(400)
     // Custom error message for Invalid rating
-    const response = await request(app).patch(`/films/${filmTwo._id}`).send({'userRating': -1}).expect(400)
+    const response = await request(app).patch(`/films/${filmThree._id}`).set(...userThreeAuth)
+    .send({'userRating': -1}).expect(400)
     expect(response.body.errors.userRating.message).toBe('Rating must be between 0 and 5.')
+})
+test('Update film should fail when not authenticated', async () => {
+    // Correct status code
+    const response = await request(app).patch(`/films/${filmThree._id}`)
+    .send({title: 'A great film'})
+    .expect(401)
+    // Correct error message
+    expect(response.body.error).toBe('Please authenticate.')
 })
 
 // Delete film
 test('Should delete film with valid id', async () => {
     // Correct status code
-    await request(app).delete(`/films/${filmTwoId}`).expect(200)
+    await request(app).delete(`/films/${filmThreeId}`).set(...userThreeAuth).expect(200)
     // Assert the database was changed correctly
-    const film = await Film.findById(filmTwoId)
+    const film = await Film.findById(filmThreeId)
     expect(film).toBeNull()
 })
-
 test('Film deletion should fail with invalid data', async () => {
     // Correct status code
-    await request(app).delete('/films/123').expect(400)
+    await request(app).delete('/films/123').set(...userThreeAuth).expect(400)
+})
+test('Film deletion should fail when not authenticated', async () => {
+    // Correct status code
+    const response = await request(app).delete(`/films/${filmThreeId}`)
+    .expect(401)
+    // Correct error message
+    expect(response.body.error).toBe('Please authenticate.')
 })
