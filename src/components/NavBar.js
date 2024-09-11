ce3e89804843fa1500b9bae67f3067f9ef71d13a
Alex Small <alex.small739@gmail.com>
2024-09-11 10:55:46 +0100

    Use current user to split logged in/out icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
+
+const NavBar = ({currentUser}) => {
+    const loggedInIcons = 
+    <>
+        <li> <NavLink to='/myfilms'>My Films</NavLink></li>
+        <li> <NavLink to={`/profile/${currentUser?.user._id}`}>Profile</NavLink></li>
+        <li> <NavLink to='/friends'>Friends</NavLink></li>
+        <li> <NavLink to='/reccomendations'>Reccomendations</NavLink></li>
+        <li> Logout</li>
+    </>
+    const loggedOutIcons = <>
+        <li> <NavLink to='/signup'>Sign up</NavLink></li>
+        <li> <NavLink to='/login'>Login</NavLink></li>
+    </>
     return (
         <div data-testid='nav-bar'>
             <NavLink to='/'><h1 data-testid='logo'>Film Friends</h1></NavLink>
             <ul>
-                <li><NavLink to='/films'>My Films</NavLink></li>
-                <li> <NavLink to='/profile'>Profile</NavLink></li>
-                <li> <NavLink to='/friends'>Friends</NavLink></li>
-                <li> <NavLink to='/reccomendations'>Reccomendations</NavLink></li>
-                <li> <NavLink to='/signup'>Sign up</NavLink></li>
-                <li> <NavLink to='/login'>Login</NavLink></li>
-                <li> Logout</li>
+                {currentUser ? loggedInIcons : loggedOutIcons}
             </ul>
             <form >
-                <label>Find your friends</label>
-                <input type='search' data-testid='friend-search'></input>
+                <label htmlFor='friend-search' id='friend-lbl'>Find your friends</label>
+                <input type='search' name='friend-search' aria-labelledby='friend-lbl'></input>
             </form>
         </div>
     )
--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -4,65 +4,140 @@
 import App from '../App'
 import { render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom'
-import user from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event'
 
 // Test all nav links and form are present
-test('Should render navigation links and friend search', () => {
-    render(<App />)
-    const logo = screen.getByTestId('logo');
-    // Assert that logo and all nav links are rendered
-    expect(logo).toBeInTheDocument();
-    expect(logo).toHaveTextContent('Film Friends');
-    const navLinksText = [
-        /My Films/, /Profile/, /^Friends$/, /Reccomendations/, /Sign up/, /Login/, /Logout/
-    ]
-    const navLinks = navLinksText.map(
-        text => screen.getByText(text)
+test('When no currentUser is provided, should render only logged out nav links', () => {
+    render(<App currentUser={null} />)
+    const loggedOutNames = [/Sign up/i, /Login/i]
+    const loggedInNames = [/My Films/i, /Profile/i, 'Friends', /Reccomendations/i]
+    // Logged out links should be present
+    loggedOutNames.map(
+        linkName => expect(screen.getByRole('link', {
+            name: linkName
+        })).toBeInTheDocument()
     )
-    navLinks.map(
-        link => expect(link).toBeInTheDocument()
+    // Logged in links should not be present
+    loggedInNames.map(
+        linkName => expect(screen.queryByRole('link', {
+            name: linkName
+        })).not.toBeInTheDocument()
     )
-    // Assert that the friend search bar is rendered
-    const friendSearchLabel = screen.getByText(/Find your friends/)
-    const friendSearchBar = screen.getByTestId('friend-search');
-    expect(friendSearchLabel).toBeInTheDocument()
-    expect(friendSearchBar).toBeInTheDocument()
+    // Logout list item should not be present
+    const logout = screen.queryByText('Logout')
+    expect(logout).not.toBeInTheDocument()
+    // Logo should be present
+    const logo = screen.getByRole('heading', {
+        name: 'Film Friends'
+    })
+    expect(logo).toBeInTheDocument()
 })
 
-// Clicking each nav link changes text on home page and changes url
-test('Clicking nav links should navigate to the correct page', async () => {
-    user.setup()
-    render(<App />)
-    // Define data used to map over nav links
-    const navLinksText = [
-        /Film Friends/, /My Films/, /Profile/, /^Friends$/, /Reccomendations/, /Sign up/, /Login/
-    ]
-    const navLinks = navLinksText.map(
-        text => screen.getByText(text)
-    )
-    const headerIds = [
-       'header-home', 'header-films','header-profile', 'header-friends','header-rec', 'header-signup', 'header-login', 
-    ]
-    // Assert appropriate heading changes when nav link is clicked
-    for (let i=0;i<7;i++){
-        await user.click(navLinks[i])
-        let header = screen.getByTestId(headerIds[i]);
-        expect(header).toBeInTheDocument()
-        // If logo is clicked assert that the film search bar also appears
-        if (i===0){
-            const filmSearchLabel = screen.getByText(/Search for a film/)
-            const filmSearchBar = screen.getByTestId('film-search');
-            expect(filmSearchLabel).toBeInTheDocument()
-            expect(filmSearchBar).toBeInTheDocument()
-        }
-        // Assert navbar is still present when navigating to a new page
-        let navBar = screen.getByTestId('nav-bar')
-        expect(navBar).toBeInTheDocument()
-        // Assert url changes when nav link is clicked
-        if (i !== 0){
-            expect(global.window.location.href).toContain(headerIds[i].slice(7))
-        } else {
-            expect(global.window.location.href).toContain('/')
-        }
+test('When currentUser is provided, should render only logged in nav links', () => {
+    const currentUser = {
+        user: {_id: '123'}
     }
+    render(<App currentUser={currentUser}/>)
+    const loggedOutNames = [/Sign up/i, /Login/i]
+    const loggedInNames = [/My Films/i, /Profile/i, 'Friends', /Reccomendations/i]
+    // Logged out links should not be present
+    loggedOutNames.map(
+        linkName => expect(screen.queryByRole('link', {
+            name: linkName
+        })).not.toBeInTheDocument()
+    )
+    // Logged in links should be present
+    loggedInNames.map(
+        linkName => expect(screen.getByRole('link', {
+            name: linkName
+        })).toBeInTheDocument()
+    )
+    // Logout list item should  be present
+    const logout = screen.getByText('Logout')
+    expect(logout).toBeInTheDocument()
+    // Logo should be present
+    const logo = screen.getByRole('heading', {
+        name: 'Film Friends'
+    })
+    expect(logo).toBeInTheDocument()
+})
+
+test('Clicking logged out nav links changes text on home page and changes url', async () => {
+    render(<App currentUser={null} />)
+    const user = userEvent.setup()
+    // Test logged out links
+    const loggedOutNames = ['Sign up', 'Login']
+    const loggedOutNavlinks = loggedOutNames.map(
+        linkName => screen.getByRole('link', {
+            name: linkName
+        })
+    )
+    let n = loggedOutNames.length
+    // Loop through links
+    for (let i=0;i<n;i++) {
+        // Corresponding heading should not be present before clicking on link
+        let heading = screen.queryByRole('heading', {
+            name: loggedOutNames[i]
+        })
+        expect(heading).not.toBeInTheDocument()
+        // Url shouldn't contain name of link
+        expect(global.window.location.href).not.toContain(loggedOutNames[i].replace(' ', '').toLowerCase())
+        // Click on link
+        await user.click(loggedOutNavlinks[i])
+        // Heading now should now be present
+        heading = screen.getByRole('heading', {
+            name: loggedOutNames[i]
+        })
+        expect(heading).toBeInTheDocument()
+        // Url should have changed to contain link name
+        expect(global.window.location.href).toContain(loggedOutNames[i].replace(' ', '').toLowerCase())
+    }
+})
+
+test('Clicking logged in nav links changes text on home page and changes url', async () => {
+    const currentUser = {
+        user: {_id: '123'}
+    }
+    render(<App currentUser={currentUser} />)
+    const user = userEvent.setup()
+    // Test logged in links
+    const loggedInNames = ['My Films', 'Profile', 'Friends', 'Reccomendations']
+    const loggedInNavlinks = loggedInNames.map(
+        linkName => screen.getByRole('link', {
+            name: linkName
+        })
+    )
+    let n = loggedInNames.length
+    // Loop through links
+    for (let i=0;i<n;i++) {
+        // Corresponding heading should not be present before clicking on link
+        let heading = screen.queryByRole('heading', {
+            name: loggedInNames[i]
+        })
+        expect(heading).not.toBeInTheDocument()
+        // Url shouldn't contain name of link
+        expect(global.window.location.href).not.toContain(loggedInNames[i].replace(' ', '').toLowerCase())
+        // Click on link
+        await user.click(loggedInNavlinks[i])
+        // Heading now should now be present
+        heading = screen.getByRole('heading', {
+            name: loggedInNames[i]
+        })
+        expect(heading).toBeInTheDocument()
+        // Url should have changed to contain link name
+        expect(global.window.location.href).toContain(loggedInNames[i].replace(' ', '').toLowerCase())
+    }
+    // Test clicking on logo takes user back to home page
+    const logo = screen.getByRole('heading', {
+        name:'Film Friends'
+    })
+    let homePageHeading = screen.queryByRole('heading', {
+        name: 'Home Page'
+    })
+    expect(homePageHeading).not.toBeInTheDocument()
+    await user.click(logo)
+    homePageHeading = screen.getByRole('heading', {
+        name: 'Home Page'
+    })
+    expect(homePageHeading).toBeInTheDocument()
 })
