5ef6859f07c88f4c48a7cb45495c6cc94e3d9694
Alex Small <alex.small739@gmail.com>
2024-09-30 22:49:36 +0100

    Generalise renderWithContext for components with a path id

--- src/test-utils/renderWithContext.jsx
+++ src/test-utils/renderWithContext.jsx
@@ -1,16 +1,41 @@
 import React from "react"
 import { CurrentUserContext } from "../contexts/CurrentUserContext"
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
-const renderWithContext = (component, currentUser = {user : {username: 'User One',_id: '123'}, token: '123'}) => {
-    return render(
-        <CurrentUserContext.Provider value={{currentUser}}>
-            <Router>
-                {component}
-            </Router>
-        </CurrentUserContext.Provider>
+// Helper function used to render a variety of components that rely on user context
+// Supplies a default user
+const renderWithContext = (component, currentUser = {user : {username: 'user two',_id: 'user2id'}, token: 'user2token'}, path = null, id = null) => {
+    
+    // If component requires a route then call createMemoryHistory to track url
+    const history = path? createMemoryHistory({ initialEntries: [`/${path}/${id}`] }) : null
+    
+    // Return an object with the rendered component and the history variable
+    return (
+        {
+            component: render(
+            /* Any component gets wrapped in user context*/
+            <CurrentUserContext.Provider value={{currentUser}}>
+                {/* If history variable is not null, wrap the component in a path and a router*/}
+                {history? (
+                    <Router history={history}>
+                        <Route path={`/${path}/:id`} render={() => component} />
+                    </Router>
+                ):(
+                    /* If history variable is null, wrap the component in a browser router*/
+                    <BrowserRouter>
+                        {component}
+                    </BrowserRouter>
+                )}
+            </CurrentUserContext.Provider>
+            /* Also return history variable to keep track of url */
+            ), history
+        }
     )
 }
+
 
 export default renderWithContext
