723658fa6b4979b8e2ff5cd442c78002d643bd2f
Alex Small <alex.small739@gmail.com>
2024-11-19 13:46:09 +0000

    Update request router to add extra data to get all requests

--- routers/request.js
+++ routers/request.js
@@ -7,11 +7,14 @@ const express = require('express')
 const router = new express.Router()
 const Request = require('../models/request')
 const auth = require('../middleware/auth')
+const User = require('../models/user')
 
 // Create a friend request 
 router.post('/requests', auth, async (req, res) => {
+    // Get sender and reciever
     const sender = req.user.id
     const reciever = req.body.reciever
+    // Check if it is possible to send a friend request
     try {
         if (reciever === sender) {
             return res.status(400).send({error: "You can't send a friend request to yourself."})
@@ -24,7 +27,10 @@ router.post('/requests', auth, async (req, res) => {
         if (reverseRequest.length) {
             return res.status(400).send({error: "This user has already sent you a friend request."})
         }
-        const request = new Request({sender, reciever, ...req.body})
+        const request = new Request({
+            sender, 
+            reciever
+        })
         await request.save()
         res.status(201).send({ request })
     } catch (e) {
@@ -38,15 +44,38 @@ router.post('/requests', auth, async (req, res) => {
 router.get('/requests', auth, async (req, res) => {
     const _id = req.user._id
     try {
-        const requests = await Request.find({$or: [{sender: _id, declined: false}, {reciever: _id, declined: false}]})        
+        const requests = await Request.find({$or: [{sender: _id}, {reciever: _id, declined: false}]})        
             .sort({accepted: 1, updatedAt: -1})
             .limit(req.query.limit)
             .skip(req.query.skip)
-        res.status(200).send({requests})
+        let fullRequests = []
+        for (let request of requests) {
+            const senderUser = await User.findById(request.sender)
+            const recieverUser = await User.findById(request.reciever)
+            fullRequests.push({
+                isSender: senderUser.username === req.user.username,
+                _id: request._id,
+                sender: {
+                    _id: request.sender,
+                    username: senderUser.username,
+                    image: senderUser.image
+                },
+                reciever: {
+                    _id: request.reciever,
+                    username: recieverUser.username,
+                    image: recieverUser.image
+                },
+                accepted: request.accepted,
+                declined: request.declined,
+                createdAt: request.createdAt,
+                updatedAt: request.updatedAt
+                })
+        }
+        res.status(200).send(fullRequests)
     } catch (e) {
         res.status(400).send(e)
     }
-})
+})            
 
 // Update request data (accept or decline)
 router.patch('/requests/:id', auth, async (req, res) => {
