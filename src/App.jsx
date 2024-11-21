d410ac98b64ffdd8a67fb3c9d24810bf5256ecda
Alex Small <alex.small739@gmail.com>
2024-11-21 14:51:25 +0000

    Add multiple parameter films route

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Films from './pages/Films';
+import Profile from './pages/profile/Profile';
+import ProfileEdit from './pages/profile/ProfileEdit';
+import Films from './pages/films/Films';
 import Friends from './pages/Friends';
-import Reccomendations from './pages/Reccomendations';
+import Reccomendations from './pages/reccomendations/Reccomendations';
 import Signup from './pages/auth/Signup';
 import Login from './pages/auth/Login';
 import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
 import {CurrentUserProvider} from './contexts/CurrentUserContext'
-import React from 'react';
+import ResetPassword from './pages/auth/PasswordReset';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <CurrentUserProvider>
-      <Router>
-        <NavBar />
-          <Switch>
-            <Route exact path='/' render={() => <Home />} />
-            <Route exact path='/myfilms' render={() => <Films  />} />
-            <Route exact path='/friends' render={() => <Friends />} />
-            <Route exact path='/reccomendations' render={() => <Reccomendations />} />
-            <Route exact path='/profile/:id' render={() => <Profile  />} />
-            <Route exact path='/signup' render={() => <Signup />} />
-            <Route exact path='/login' render={() => <Login />} />
-          </Switch>
-      </Router>
-    </CurrentUserProvider>
-  );
+    <div className={styles.App}>
+      <CurrentUserProvider>
+        <Router>
+          <NavBar />
+            <Switch>
+              <Route exact path='/' render={() => <Home />} />
+              <Route exact path='/films/:id' render={() => <Films />} />
+              <Route exact path='/films/:id/:imdbID/:database' render={() => <Films />} />
+              <Route exact path='/friends/' render={() => <Friends />} />
+              <Route exact path='/reccomendations' render={() => <Reccomendations />} />
+              <Route exact path='/profile/:id' render={() => <Profile  />} />
+              <Route exact path='/profile/edit/:id' render={() => <ProfileEdit  />} />
+              <Route exact path='/profile/edit/password/:id' render={() => <ResetPassword />} />
+              <Route exact path='/signup' render={() => <Signup />} />
+              <Route exact path='/login' render={() => <Login />} />
+            </Switch>
+        </Router>
+      </CurrentUserProvider>
+    </div>
+  );   
 }
 
 export default App;
