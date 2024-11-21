ff4f8796c5fadbc367b2d94070a1c2fd933d2aa0
Alex Small <alex.small739@gmail.com>
2024-11-21 14:52:46 +0000

    Move button functionality to FriendRequestButtons

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,48 +1,19 @@
-import React from 'react';
-import { Button, Image } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Button, Col, Image, Row, OverlayTrigger, Tooltip, Modal } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-
 /* 
 Component used in friends page
-Displays username, profile image, and an action button
-Action button can be: send reccomendation, accept/decline request or text
+Displays username, profile image
 */
-const User = ({user, id, status, updateRequest, sendRequest}) => {
-
+const User = ({data, searchResult}) => {
     const history = useHistory()
-    // Determine action button based on friend request status
-    const getActionComponent = () => {
-        if (status === 'accepted') {
-            return (
-            <>
-                    <Button onClick={() => history.push(`/reccomendations/send/users/${user._id}`)}>
-                        <i className="fa-solid fa-share"></i>
-                    </Button>
-            </>)
-        }
-        if (status === 'sent') {
-            return 'Friend request sent'
-        }
-        if (status === 'recieved') {
-            return (
-                <>
-                    <p>Wants to be friends</p>
-                    <Button variant='outline-secondary' onClick={() => updateRequest(true, id)}>Accept</Button>
-                    <Button variant='outline-secondary' onClick={() => updateRequest(false, id)}>Decline</Button>  
-                </>
-            )
-        }
-        return (
-            <Button onClick={() => sendRequest(user._id)} variant='outline-secondary'>Add Friend</Button>
-        )
-    }
-    // Render profile image, username and action component
+    
+    // Render profile image, username
     return (
-        <span>
-            <Image roundedCircle src={user.image} width={50} />
-            {user.username}
-            {getActionComponent()}
-        </span>
+        <Row onClick={() => history.push(`/profile/${data._id}`)}>
+            <Col md={searchResult? 3 : 3}><Image roundedCircle src={data.image} width={searchResult? 50: 70} /></Col>
+            <Col md={searchResult? 6 : 9} style={{textAlign:'left', paddingTop: searchResult? '3%':'7%'}} >{data.username}</Col>
+        </Row>
     )
 }
 
