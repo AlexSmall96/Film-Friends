9f32c0f4c789fec594f16ae2f1de4202efa81e5a
Alex Small <alex.small739@gmail.com>
2024-11-21 16:59:07 +0000

    Add setHasLoaded state setter

--- src/components/ResultsPagination.jsx
+++ src/components/ResultsPagination.jsx
@@ -3,32 +3,42 @@ import Pagination from 'react-bootstrap/Pagination';
 
 // Renders a custom pagination component depending on the number of pages and current page selected
 // First, prev, ellipsis, next and last are only required for certain values of current page and final page //
-const ResultsPagination = ({currentPage, setCurrentPage, finalPage}) => {
+const ResultsPagination = ({currentPage, setCurrentPage, finalPage, setHasLoaded}) => {
+
+    const handleClick = (page) => {
+        setCurrentPage(page)
+        if (setHasLoaded){
+            setHasLoaded(false)
+        }
+    }
+
     return (
         <Pagination>
             {currentPage !== 1? (
                 <>
-                    <Pagination.First onClick={() => setCurrentPage(1)} />
+                    <Pagination.First onClick={() => handleClick(1)} />
                         {currentPage !== 2? (
                             <>
-                                <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)}/>
+                                <Pagination.Prev onClick={() => handleClick(currentPage - 1)}/>
                                 <Pagination.Ellipsis />
                             </>
                         ):('')}
-                    <Pagination.Item onClick={() => setCurrentPage(currentPage - 1)}>{currentPage - 1}</Pagination.Item>
+                    <Pagination.Item onClick={() => handleClick(currentPage - 1)}>{currentPage - 1}</Pagination.Item>
                 </>
             ):('')}
             <Pagination.Item active>{currentPage}</Pagination.Item>
             {currentPage !== finalPage? (
                 <>
-                    <Pagination.Item onClick={() => setCurrentPage(currentPage + 1)}>{currentPage + 1}</Pagination.Item>
+                    <Pagination.Item onClick={() => handleClick(currentPage + 1)}>
+                        {currentPage + 1}
+                    </Pagination.Item>
                     {currentPage !== finalPage - 1? (
                         <>
                             <Pagination.Ellipsis />
-                            <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)}/>
+                            <Pagination.Next onClick={() => handleClick(currentPage + 1)}/>
                         </>
                     ):('')}
-                    <Pagination.Last onClick={() => setCurrentPage(finalPage)}/>
+                    <Pagination.Last onClick={() => handleClick(finalPage) }/>
                 </>
             ):('')}
         </Pagination>
