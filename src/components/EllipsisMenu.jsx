0f97b86c96fe7c13159147b6916864ea9e57e2b3
Alex Small <alex.small739@gmail.com>
2024-11-25 15:32:41 +0000

    Tidy up and comment

--- src/components/EllipsisMenu.jsx
+++ src/components/EllipsisMenu.jsx
@@ -6,14 +6,20 @@ import { CustomMenu, CustomToggle } from './CustomDropDown';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 import { axiosReq } from '../api/axiosDefaults';
 
-const EllipsisMenu = ({handleDelete, handleShare, updateViewingData, viewingData, omdbData, filmId}) => {
-    const history = useHistory()
+/* 
+Ellipsis menu - used as a subcomponent of the film component when rendered on films page
+Allows user to share film, delete film, or make public / private
+*/
+const EllipsisMenu = ({handleDelete, updateViewingData, viewingData, omdbData, filmId}) => {
+    // Contexts
     const {currentUser} = useCurrentUser()
+    // Hooks
+    const history = useHistory()
+    // Initialise state variables
     const [showMenu, setShowMenu] = useState(false);
     const [showModal, setShowModal] = useState(false)
     const target = useRef(null)
     const [message, setMessage] = useState("Hey! Check out this awesome film I've just watched. I think you'll love it! ")
-    const [feedback, setFeedback] = useState('')
     const [recipient, setRecipient] = useState(null)
     const [friends, setFriends] = useState([])
     const [allFriends, setAllFriends] = useState([])
@@ -21,44 +27,57 @@ const EllipsisMenu = ({handleDelete, handleShare, updateViewingData, viewingData
     const [sent, setSent] = useState(false)
     const [hasLoaded, setHasLoaded] = useState(false)
 
+    // Reset variables for new modal being shown
     const handleShowModal = () => {
         setShowModal(true)
         setShowMenu(false)
         setSent(false)
     }
 
+    // Change handler for message form
     const handleMessageChange = (event) => {
         setMessage(event.target.value)
     }
 
+    // Handle close modal
     const handleCloseModal = () => setShowModal(false)
 
+    // Use effect executes whenever a film is shared or a new film is selected
     useEffect(() => {
         const fetchRequests = async () => {
-            const response = await axiosReq.get(`/requests/?limit=10000&skip=0`, {headers: {'Authorization': `Bearer ${currentUser.token}`}})
-            const acceptedRequests = response.data.filter(req => req.accepted)
+            setHasLoaded(false)
+            // Get all users friend requests to determine who film can be shared with
+            const response = await axiosReq.get(`/requests/`, {headers: {'Authorization': `Bearer ${currentUser.token}`}})
+            // Filter to accepted requests (friends)
+            const acceptedRequests = await response.data.filter(req => req.accepted)
+            // Get all reccomendations to determine who film has already been shared with
             const reccomendationsResponse = await axiosReq.get('/reccomendations', {headers: {'Authorization': `Bearer ${currentUser.token}`}})
-            const alreadyReccomended = reccomendationsResponse.data.fullReccomendations.filter(rec => rec.film.imdbID === omdbData.imdbID)
-            const alreadyReccomendedUsernames = alreadyReccomended.map(rec => rec.reciever.username)
-            setFriends(acceptedRequests.filter(request => request.isSender? !alreadyReccomendedUsernames.includes(request.reciever.username):!alreadyReccomendedUsernames.includes(request.sender.username)))
+            const alreadyReccomended = await reccomendationsResponse.data.fullReccomendations.filter(rec => rec.film?.imdbID === omdbData?.imdbID)
+            const alreadyReccomendedUsernames = await alreadyReccomended.map(rec => rec.reciever.username)
+            // Filter friend requests based on already sent reccomendations
+            const filteredRequests = await acceptedRequests.filter(request => request.isSender? !alreadyReccomendedUsernames.includes(request.reciever.username):!alreadyReccomendedUsernames.includes(request.sender.username))
+            setFriends(filteredRequests)
             setAllFriends(acceptedRequests)
             setHasLoaded(true)
         }
         fetchRequests()
     }, [currentUser.token, updated, omdbData])
 
+    // Handle send - creates a new reccomendation
     const handleSend = async () => {
         try {
+            // Post data
             await axiosReq.post('/reccomendations', {
                 film: filmId,
                 sender: currentUser.user._id,
                 reciever: recipient._id,
                 message
             }, {headers: {'Authorization': `Bearer ${currentUser.token}`}})
-            setUpdated(!updated)
+            // Reset state variables and trigger useEffect re-run
             setRecipient(null)
             setSent(true)
             setHasLoaded(false)
+            setUpdated(!updated)
         } catch (err) {
             console.log(err)
         }
@@ -66,97 +85,103 @@ const EllipsisMenu = ({handleDelete, handleShare, updateViewingData, viewingData
 
     return (
         <>
-        <div style={{float: 'right'}}>
-            <i onClick={() => setShowMenu(!showMenu)} ref={target} className={`fa-xl fa-solid fa-ellipsis-vertical`}></i>
-            <Overlay 
-                target={target.current} 
-                show={showMenu} 
-                placement="left" 
-                rootClose='false'
-                onHide={() => {setShowMenu(false)}} 
-            >
-            {({placement: _placement, arrowProps: _arrowProps, show: _show, popper: _popper, hasDoneInitialMeasure: _hasDoneInitialMeasure, ...props}) => 
-                (<div 
-                    {...props}
-                    style={{
-                        position: 'absolute',
-                        backgroundColor: 'lightgrey',
-                        padding: '2px 10px',
-                        color: 'black',
-                        borderRadius: 3,
-                        ...props.style,
-                    }}
+            <div style={{float: 'right'}}>
+                {/* ELLIPSIS ICON TO SHOW MENU */}
+                <i onClick={() => setShowMenu(!showMenu)} ref={target} className={`fa-xl fa-solid fa-ellipsis-vertical`}></i>
+                {/* OVERLAY TO SHOW OPTIONS */}
+                <Overlay 
+                    target={target.current} 
+                    show={showMenu} 
+                    placement="left" 
+                    rootClose='false'
+                    onHide={() => {setShowMenu(false)}} 
                 >
-                    {friends.length? 
-                        <p onClick={handleShowModal} className={appStyles.hover}><i className="fa-solid fa-share"></i> Share</p>
-                    : allFriends.length? 
-                        <p style={{color: 'grey'}}><i className="fa-solid fa-share"></i> You've shared this with all your friends</p>
-                    :   <p style={{color: 'grey'}}><i className="fa-solid fa-share"></i> You don't have any friends yet</p>}
-                    <p className={appStyles.hover} onClick={handleDelete}><i className="fa-regular fa-trash-can"></i> Remove from Watchlist</p>
-                    <p className={appStyles.hover} onClick={() => updateViewingData(null, null, !viewingData.public)}><i className="fa-solid fa-pen"></i> {viewingData.public? 'Make Private': 'Make Public'}</p>
-                </div>)
-            }
-            </Overlay>
-        </div>
-              <Modal show={showModal} onHide={handleCloseModal}>
-              <Modal.Header closeButton>
-            {hasLoaded? 
-              recipient? 
-                <p>Sending to: </p>
-            :
-                <p>{sent? friends.length? 'Film Shared. Select another user to share.' :`Film Shared. You've shared this film with all your friends.`: ''}</p>
-            
-            :''}
-            {hasLoaded?
-              friends.length?
-              <Dropdown>
-              <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-              {recipient?.username || 'Select recipient'}
-              </Dropdown.Toggle>
-              <Dropdown.Menu as={CustomMenu}>
-                  <div style={{maxHeight: '400px', overflowY: 'scroll'}}>
-                  {friends.map(
-                      friend => <Dropdown.Item key={friend._id} 
-                                  onClick={() => setRecipient(friend.isSender? friend.reciever : friend.sender)}>
-                                      {friend.isSender? friend.reciever.username : friend.sender.username}
-                                  </Dropdown.Item>
-                  )}
-                  </div>
-
-              </Dropdown.Menu>
-              </Dropdown>
-              : 
-              ''
-            :''}
-            </Modal.Header>
-            <Modal.Body>
+                    {({placement: _placement, arrowProps: _arrowProps, show: _show, popper: _popper, hasDoneInitialMeasure: _hasDoneInitialMeasure, ...props}) => 
+                        (<div 
+                            {...props}
+                            style={{
+                                position: 'absolute',
+                                backgroundColor: 'lightgrey',
+                                padding: '2px 10px',
+                                color: 'black',
+                                borderRadius: 3,
+                                ...props.style,
+                            }}
+                        >   
+                            {/* SHARE, REMOVE AND MAKE PUBLIC / PRIVATE OPTIONS */}
+                            <p onClick={handleShowModal} className={appStyles.hover}><i className="fa-solid fa-share"></i> Share</p>
+                            <p className={appStyles.hover} onClick={handleDelete}><i className="fa-regular fa-trash-can"></i> Remove from Watchlist</p>
+                            <p className={appStyles.hover} onClick={() => updateViewingData(null, null, !viewingData.public)}><i className="fa-solid fa-pen"></i> {viewingData.public? 'Make Private': 'Make Public'}</p>
+                        </div>)
+                    }
+                </Overlay>
+            </div>
+            { /* MODAL FOR SHARE FILM */}
+            <Modal show={showModal} onHide={handleCloseModal}>
+                {/* SELECT RECIPIENT DROPDOWN */}
+                <Modal.Header closeButton>
+                    {hasLoaded?
+                        allFriends.length? 
+                            friends.length? (
+                                <>
+                                    <p>{recipient? 'Sending to' : sent? 'Film Shared. Select another user to share.': ''}</p>
+                                    <Dropdown>
+                                        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                                            {recipient?.username || 'Select recipient'}
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu as={CustomMenu}>
+                                            <div style={{maxHeight: '400px', overflowY: 'scroll'}}>
+                                                {friends.map(friend => 
+                                                    <Dropdown.Item key={friend._id} 
+                                                        onClick={() => setRecipient(friend.isSender? friend.reciever : friend.sender)}
+                                                    >
+                                                        {friend.isSender? friend.reciever.username : friend.sender.username}
+                                                    </Dropdown.Item>
+                                                )}
+                                            </div>
+                                        </Dropdown.Menu>
+                                    </Dropdown>                                
+                                </>
+                            ):(
+                                "You've shared this film with all your friends."
+                            )
+                        :
+                            <>
+                                You don't have any friends to share this film with
+                                <Button variant='link' onClick={() => history.push('/friends')}>Search for users</Button>
+                            </>
+                        
+                    :(
+                        ''
+                    )}
+                </Modal.Header>
+                {/* FILM TITLE AND IMAGE IN BODY */}
+                <Modal.Body>
                 {hasLoaded? (
-                <>
-                    <Image src={omdbData.Poster} width={50}/>
-                    {omdbData.Title}
-                </>
-                ):(
-                    <Spinner />
-                )}
-
-            </Modal.Body>
-            <Modal.Footer>
-                  <Form>
-                      <Form.Label>Message</Form.Label>
-                      <Form.Control style={!recipient? {color: 'grey'}:{color: 'black'}} readOnly={recipient === null} value={message} onChange={handleMessageChange}>
-
-                      </Form.Control>
-                  </Form>
-              <Button variant="secondary" onClick={handleCloseModal}>
-                Close
-              </Button>
-              <Button disabled={recipient === null} variant="primary" onClick={handleSend}>
-                {sent && !hasLoaded ? 'Sending...' : 'Send'}
-              </Button>
-            </Modal.Footer>
-            
+                    <>
+                        <Image src={omdbData.Poster} width={50}/>
+                        {omdbData.Title}
+                    </>
+                    ):(
+                        <Spinner />
+                    )}
+                </Modal.Body>
+                {/* SEND AND CLOSE BUTTONS */}
+                <Modal.Footer>
+                    <Form>
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control style={!recipient? {color: 'grey'}:{color: 'black'}} readOnly={recipient === null} value={message} onChange={handleMessageChange}>
+                        </Form.Control>
+                    </Form>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                    <Button disabled={recipient === null} variant="primary" onClick={handleSend}>
+                        {sent && !hasLoaded ? 'Sending...' : 'Send'}
+                    </Button>
+                </Modal.Footer>
             </Modal>
-            </>
+        </>
     )
 }
 
