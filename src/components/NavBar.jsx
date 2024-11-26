4bd0e117246188121e7ef01e21ec03b0f61c2a0a
Alex Small <alex.small739@gmail.com>
2024-11-26 15:51:25 +0000

    Add profile related pages to dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { axiosReq } from '../api/axiosDefaults';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
-import { Container, Navbar, Nav, Image, Row,} from 'react-bootstrap'
+import { Container, Navbar, Nav, Image, Row, Form, Dropdown, Toast,  ToastContainer, Button, Overlay, NavDropdown} from 'react-bootstrap'
+import User from './User';
 
 const NavBar = () => {
+    const [searchResults, setSearchResults] = useState([])
     const { currentUser, setCurrentUser  } = useCurrentUser()
+    const [search, setSearch] = useState('')
+    const [showMenu, setShowMenu] = useState(false);
+    const target = useRef(null)
+
     const handleLogout = async () => {
-      localStorage.clear()
+        localStorage.clear()
         try {
             await axiosReq.post('/users/logout', {}, {
                 headers: {'Authorization': `Bearer ${currentUser?.token}`}
@@ -20,16 +26,22 @@ const NavBar = () => {
     const loggedInIcons = 
     <>
         <Nav.Link href={`/films/${currentUser?.user._id}`}>My Films</Nav.Link>
-        <Nav.Link href={`/profile/${currentUser?.user._id}`}>Profile</Nav.Link>
         <Nav.Link href='/friends'>Friends</Nav.Link>
         <Nav.Link href='/reccomendations'>Reccomendations</Nav.Link>
-        <Nav.Link href='/' onClick={handleLogout}>Logout</Nav.Link>
+        <NavDropdown title={<Image src={currentUser?.user.image} width={50} roundedCircle/>} id="basic-nav-dropdown">
+            <NavDropdown.Item href={`/profile/${currentUser?.user._id}`}><i className="fa-solid fa-user"></i> Profile</NavDropdown.Item>
+            <NavDropdown.Item href={`/profile/edit/${currentUser?.user._id}`}><i className="fa-solid fa-pen-to-square"></i> Edit Profile</NavDropdown.Item>
+            <NavDropdown.Item onClick={handleLogout} href="/"><i className="fa-solid fa-right-from-bracket"></i> Logout</NavDropdown.Item>
+            <NavDropdown.Item href={`/profile/delete/${currentUser?.user._id}`}><i className="fa-solid fa-trash-can"></i> Delete Account</NavDropdown.Item>
+        </NavDropdown>
     </>
+
     const loggedOutIcons = 
     <>
         <Nav.Link href='/signup'>Sign up</Nav.Link>
         <Nav.Link href='/login'>Login</Nav.Link>
     </>
+
     return (
         <Navbar bg="light" data-bs-theme="light">
           	<Container>
