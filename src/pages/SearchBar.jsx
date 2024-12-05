399ff1966c45728449af0c1a67f10a4da13560bf
Alex Small <alex.small739@gmail.com>
2024-12-05 12:11:58 +0000

    Refactor using custom search bar and custom css

--- src/pages/SearchBar.jsx
+++ src/pages/SearchBar.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import React, { useEffect, useState} from 'react'
 import { axiosReq } from '../api/axiosDefaults'
-import { Button, Container, Row, Col, Form } from 'react-bootstrap'
+import { Button, Container, Row, Col, Form,} from 'react-bootstrap'
 import appStyles from '../App.module.css'
+import styles from '../styles/Home.module.css'
+import './CustomSearchBar.module.css'
 
 /* 
 The search suggestions functionality was inspired by the following article
@@ -10,43 +11,35 @@ https://www.dhiwise.com/post/how-to-build-react-search-bar-with-suggestions#cust
 */
 
 const SearchBar = ({setSearchResults, setTotalResults, currentPage, setFinalPage, setError}) =>{
-
+	
 	// Initialize state variables
 	const [search, setSearch] = useState('')
 	const [imdbID, setImdbID] = useState('')
 	const [suggestions, setSuggestions] = useState([])
+	const [showSuggestions, setShowSuggestions] = useState(false);
 	const [submitted, setSubmitted] = useState(false)
 
 	// Get search results from OMDB API to use as suggestions
-  	const handleOnSearch = async (string) => {
-		const response = await axiosReq.get(`filmSearch/?search=${string.trim()}`)
+  	const handleChange = async (event) => {
+		const string = event.target.value
+		setSearch(string)
+		setSuggestions([])
+		if (string.length >= 5){
+			const response = await axiosReq.get(`filmSearch/?search=${string.trim()}`)
 			if (response?.data?.Search?.length){
 				const suggestionsArray = response.data.Search.map(
 					(result) => {
-						return {id: result.imdbID, name: result.Title}
+						return {id: result.imdbID, Title: result.Title}
 					}
 				)
 				setSearch(string)
+				setShowSuggestions(true)
 				return setSuggestions(suggestionsArray)
 			}
+			setShowSuggestions(false)
 			setSuggestions([])
-			setSearch(string)
+		}
   	}
-
-	// If a suggestion is selected, set name and imdb as search values
-	const handleOnSelect = async (item) => {
-		setSearch(item.name)
-		setImdbID(item.id)
-	}
-
-	// Formatting for suggestions
-  	const formatResult = (item) => {
-		return (
-			<>
-				<span style={{textAlign: 'left' }}>{item.name}</span>
-			</>
-		);
-  	};
 
 	// Use effect to make a call to OMDB API to get actual results
 	// Triggered by current page or submitted boolean variable changing
@@ -88,35 +81,66 @@ const SearchBar = ({setSearchResults, setTotalResults, currentPage, setFinalPage
 		fetchData()
 	}, [submitted, currentPage])
 
-
+	// Handle submit to fetch search results
 	const handleSubmit = (event) => {
 		event?.preventDefault()
+		setShowSuggestions(false)
 		setSubmitted(!submitted)
 	}
 
+	// Handle click - sets search query and hides suggestions
+	const handleClick = (event) => {
+		// Check if a suggestion has been clicked 
+		if (event.target.classList.contains('suggestion')){
+			// Set search query with suggestion data
+			setSearch(event.target.innerHTML)
+			setImdbID(event.target.id)
+			// Hide suggestions
+			setShowSuggestions(false)
+			// Submit form to trigger useEffect
+			setSubmitted(!submitted)
+		} else {
+			// Hide suggestions
+			setShowSuggestions(false)
+		}
+	}
+
+	document.addEventListener('mouseup', handleClick)
   	return (
 		<>
 			{/* FORM  */}
 			<Form onSubmit={handleSubmit}>
-				<Container className={appStyles.bigVerticalMargin}>
+				<Container className={`${appStyles.bigVerticalMargin}`}>
 					<Row>
 						<Col xs={10} sm={10} md={11} className={appStyles.noPadding}>
 						{/* SEARCH BAR  */}
-						<ReactSearchAutocomplete
-							items={suggestions}
-							onSearch={handleOnSearch}
-							onSelect={handleOnSelect}
-							autoFocus
-							fuseOptions={{minMatchCharLength: 3}}
-							showNoResults={false}
-							styling={{borderRadius: '1rem 0 0 1rem', marginRight: '0px', height: '50px', boxShadow: '0 0 0 0'}}
-							formatResult={formatResult}
-							placeholder="Search for a film"
-						/>				
+						<Form.Control 
+							type='text' 
+							placeholder='Search for a film' 
+							style={{height: '50px', borderRadius: '1rem 0 0 1rem'}} 
+							value={search}
+							onChange={handleChange}
+						/>
 						</Col>
 						{/* SEARCH BUTTON */}
 						<Col xs={2} sm={2} md={1} className={appStyles.noPadding} >
-							<Button type='submit' variant='outline-secondary' style={{borderRadius: '0 1rem 1rem 0', height: '52px', width: '100%'}}><i className="fa-solid fa-magnifying-glass"></i></Button>
+							<Button type='submit' variant='outline-secondary' className={styles.searchButton}><i className="fa-solid fa-magnifying-glass"></i></Button>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={10} sm={10} md={11} className={`${appStyles.noPadding} ${styles.suggestions}`}>
+							{suggestions.length && showSuggestions? 
+								suggestions.map(suggestion =>
+									<p 
+									    id={suggestion.id}
+										key={suggestion.id}
+										className='suggestion'
+										// onClick={() => handleClick(suggestion.Title, suggestion.id)}
+									>
+										{suggestion.Title}
+									</p>
+								)
+							:''}
 						</Col>
 					</Row>
 				</Container>
