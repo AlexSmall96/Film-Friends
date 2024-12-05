dddd7d2464ec029653234e756cedcef02d627893
Alex Small <alex.small739@gmail.com>
2024-12-05 17:13:20 +0000

    Refactor to use a constant 10 buttons

--- src/components/ResultsPagination.jsx
+++ src/components/ResultsPagination.jsx
@@ -1,48 +1,56 @@
-import React from 'react';
-import Pagination from 'react-bootstrap/Pagination';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import appStyles from '../App.module.css'
 
 // Renders a custom pagination component depending on the number of pages and current page selected
-// First, prev, ellipsis, next and last are only required for certain values of current page and final page //
 const ResultsPagination = ({currentPage, setCurrentPage, finalPage, setHasLoaded}) => {
-
-    const handleClick = (page) => {
-        setCurrentPage(page)
-        if (setHasLoaded){
-            setHasLoaded(false)
+   
+    const [pages, setPages] = useState([])
+    // Use current page and final page to determine pages array
+    useEffect(() => {
+        const items = []
+        if (finalPage <= 10) {
+            for (let i=1;i<=finalPage;i++){
+                items.push(i)
+            }
+        } else {
+            if (currentPage <= 3) {
+                for (let i=1;i<=8;i++){
+                    items.push(i)
+                }
+                items.push('...')
+                items.push(finalPage)
+            } else if (currentPage <= finalPage - 8){
+                items.push(1)
+                items.push('...')
+                for (let i=currentPage;i<=currentPage + 5;i++){
+                    items.push(i)
+                }
+                items.push('...')
+                items.push(finalPage)
+            } else {
+                items.push(1)
+                items.push('...')
+                for (let i=finalPage-7;i<=finalPage-1;i++){
+                    items.push(i)
+                }
+                items.push(finalPage)
+            }
         }
-    }
+        setPages(items)
+    }, [currentPage, finalPage])
 
     return (
-        <Pagination  >
-            {currentPage !== 1? (
-                <>
-                    <Pagination.First linkStyle={{color: 'black'}} onClick={() => handleClick(1)} />
-                        {currentPage !== 2? (
-                            <>
-                                <Pagination.Prev linkStyle={{color: 'black'}} onClick={() => handleClick(currentPage - 1)}/>
-                                <Pagination.Ellipsis linkStyle={{color: 'black'}} />
-                            </>
-                        ):('')}
-                    <Pagination.Item linkStyle={{color: 'black'}} onClick={() => handleClick(currentPage - 1)}>{currentPage - 1}</Pagination.Item>
-                </>
-            ):('')}
-            <Pagination.Item linkStyle={{color: 'white', backgroundColor: 'grey', borderColor: 'grey'}} active>{currentPage}</Pagination.Item>
-            {currentPage !== finalPage? (
-                <>
-                    <Pagination.Item linkStyle={{color: 'black'}} onClick={() => handleClick(currentPage + 1)}>
-                        {currentPage + 1}
-                    </Pagination.Item>
-                    {currentPage !== finalPage - 1? (
-                        <>
-                            <Pagination.Ellipsis linkStyle={{color: 'black'}}/>
-                            <Pagination.Next  linkStyle={{color: 'black'}} onClick={() => handleClick(currentPage + 1)}/>
-                        </>
-                    ):('')}
-                    <Pagination.Last linkStyle={{color: 'black'}} onClick={() => handleClick(finalPage) }/>
-                </>
-            ):('')}
-        </Pagination>
+        <>
+            {/* MAP PAGES ARRAY TO BUTTONS */}
+            <Button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)} variant='link'><i className="fa-solid fa-chevron-left"></i></Button>
+                {pages.map(page => 
+                    <Button className={appStyles.noUnderline} variant={page === currentPage? 'secondary': 'link'} onClick={() => setCurrentPage(page)}>{page}</Button>
+                )}
+            <Button disabled={currentPage === finalPage} onClick={() => setCurrentPage(currentPage + 1)} variant='link'><i className="fa-solid fa-chevron-right"></i></Button>         
+        </>
     )
+
 }
 
 export default ResultsPagination
