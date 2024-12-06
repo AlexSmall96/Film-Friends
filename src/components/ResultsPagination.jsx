986b09decc509796f40bba2ef8218f34fdb74536
Alex Small <alex.small739@gmail.com>
2024-12-06 14:21:58 +0000

    Use window dimensions to make number of page links variable

--- src/components/ResultsPagination.jsx
+++ src/components/ResultsPagination.jsx
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import appStyles from '../App.module.css'
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 // Renders a custom pagination component depending on the number of pages and current page selected
-const ResultsPagination = ({currentPage, setCurrentPage, finalPage, setHasLoaded}) => {
-   
+// Shows a certain number of page links depending on window size
+const ResultsPagination = ({currentPage, setCurrentPage, finalPage}) => {
+    const {width} = useWindowDimensions()
     const [pages, setPages] = useState([])
     // Use current page and final page to determine pages array
     useEffect(() => {
+        const show = width >= 576? 10 : 6
         const items = []
-        if (finalPage <= 10) {
+        if (finalPage <= show) {
             for (let i=1;i<=finalPage;i++){
                 items.push(i)
             }
         } else {
             if (currentPage <= 3) {
-                for (let i=1;i<=8;i++){
+                for (let i=1;i<=show-2;i++){
                     items.push(i)
                 }
                 items.push('...')
                 items.push(finalPage)
-            } else if (currentPage <= finalPage - 8){
+            } else if (currentPage <= finalPage - show + 2){
                 items.push(1)
                 items.push('...')
-                for (let i=currentPage;i<=currentPage + 5;i++){
+                for (let i=currentPage;i<=currentPage + show - 5;i++){
                     items.push(i)
                 }
                 items.push('...')
@@ -31,23 +34,28 @@ const ResultsPagination = ({currentPage, setCurrentPage, finalPage, setHasLoaded
             } else {
                 items.push(1)
                 items.push('...')
-                for (let i=finalPage-7;i<=finalPage-1;i++){
+                for (let i=finalPage-show + 3;i<=finalPage-1;i++){
                     items.push(i)
                 }
                 items.push(finalPage)
             }
         }
         setPages(items)
-    }, [currentPage, finalPage])
+    }, [currentPage, finalPage, width])
 
     return (
         <>
             {/* MAP PAGES ARRAY TO BUTTONS */}
-            <Button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)} variant='link'><i className="fa-solid fa-chevron-left"></i></Button>
+            {currentPage !== 1?
+                <Button size='sm' onClick={() => setCurrentPage(currentPage - 1)} variant='link'><i className="fa-solid fa-chevron-left"></i></Button>
+            :''
+            }
                 {pages.map(page => 
-                    <Button className={appStyles.noUnderline} variant={page === currentPage? 'secondary': 'link'} onClick={() => setCurrentPage(page)}>{page}</Button>
+                    <Button key={page} className={appStyles.noUnderline} variant={page === currentPage? 'secondary': 'link'} onClick={() => setCurrentPage(page)}>{page}</Button>
                 )}
-            <Button disabled={currentPage === finalPage} onClick={() => setCurrentPage(currentPage + 1)} variant='link'><i className="fa-solid fa-chevron-right"></i></Button>         
+            {currentPage !== finalPage?
+                <Button size='sm' disabled={currentPage === finalPage} onClick={() => setCurrentPage(currentPage + 1)} variant='link'><i className="fa-solid fa-chevron-right"></i></Button>  
+            :''}   
         </>
     )
 
