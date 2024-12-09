083f1c2d70ecc9e3d95d3190ec2fc41394e3fecf
Alex Small <alex.small739@gmail.com>
2024-12-09 18:31:08 +0000

    Move sort and filter buttons into button group

--- src/pages/films/Filters.jsx
+++ src/pages/films/Filters.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useRef} from 'react';
-import { Button, Col, Container, Row, Dropdown, DropdownButton, Spinner } from 'react-bootstrap';
+import { Button, Col, Container, Row, Dropdown, DropdownButton, Spinner, ButtonGroup } from 'react-bootstrap';
 import styles from '../../styles/Films.module.css'
 import appStyles from '../../App.module.css'
 import { useCurrentFilm } from '../../contexts/CurrentFilmContext';
@@ -11,10 +11,10 @@ const Filters = ({isOwner, filter, setFilter, sort, setSort, username, mobile, f
         omdbData
     } = useCurrentFilm()
     return (
-        <>
-        <Row>
+        <Container fluid>
+        <Row style={{width: '40%', margin: 'auto'}}> 
         {mobile? (
-                <DropdownButton className={styles.filter}  variant='light' title={omdbData.Title}>
+                <DropdownButton size="md" variant='outline-secondary' className={styles.filter}  title={omdbData.Title}>
                     {filteredFilms.map(
                         film => 
                             <Dropdown.Item 
@@ -34,35 +34,52 @@ const Filters = ({isOwner, filter, setFilter, sort, setSort, username, mobile, f
 
         </Row>
         <Row>
-            <Col xs={6} sm={4} md={12}>
-            {isOwner? (
-                
-                    <DropdownButton className={styles.filter} variant='outline-secondary' title={`Your ${filter.public? 'Public': 'Private'} Watchlist`}>
-                        <Dropdown.Item onClick={!filter.public ? () => setFilter({public: true, watched: filter.watched}): null}>Public</Dropdown.Item>
-                        <Dropdown.Item onClick={filter.public ? () => setFilter({public: false, watched: filter.watched}): null}>Private</Dropdown.Item>
+            <Col xl={6} lg={12} md={12} sm={6} xs={6}>
+                {isOwner? ( 
+                        <DropdownButton size="md" className={appStyles.verticalMargin} style={{width: '100%'}} variant='outline-secondary' title={`Your ${filter.public? 'Public': 'Private'} Watchlist`}>
+                            <Dropdown.Item onClick={!filter.public ? () => setFilter({public: true, watched: filter.watched}): null}>Public</Dropdown.Item>
+                            <Dropdown.Item onClick={filter.public ? () => setFilter({public: false, watched: filter.watched}): null}>Private</Dropdown.Item>
+                        </DropdownButton>
+                    
+                ):(
+                    `${username}'s Watchlist`
+                )}            
+            </Col>
+            <Col xl={6} lg={12} md={12} sm={6} xs={6}>
+                <ButtonGroup>
+                    <DropdownButton 
+                        as={ButtonGroup} 
+                        size="md" 
+                        className={`${appStyles.verticalMargin}`} 
+                        variant='outline-secondary' 
+                        title={
+                            <>
+                                <i className="fa-solid fa-sort"></i> {sort}
+                            </>
+                        }
+                    >
+                        <Dropdown.Item onClick={() => setSort('A-Z')}>A-Z</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setSort('Last Updated')}>Last Updated</Dropdown.Item>
                     </DropdownButton>
-                
-            ):(
-                `${username}'s Watchlist`
-            )}
-            </Col>
-            <Col xs={6} sm={6} md={12}>
-            <div style={{display: 'block', margin: '5px 0px 5px 0px'}}>
-                <DropdownButton className={`${styles.filmSortButton} ${styles.filter} ${appStyles.smallFont}`} variant='outline-secondary' title={sort === 'title'? 'A-Z': 'Last updated'}>
-                    <Dropdown.Item onClick={() => setSort('A-Z')}>A-Z</Dropdown.Item>
-                    <Dropdown.Item onClick={() => setSort('')}>Last Updated</Dropdown.Item>
-                </DropdownButton>
-                <DropdownButton 
-                className={`${styles.filmSortButton} ${styles.filter} ${appStyles.smallFont}`} variant='outline-secondary' title={filter.watched}>
-                    <Dropdown.Item onClick={filter.watched !== 'All' ? () => {setFilter({public: filter.public, watched: 'All'})} : null}>All</Dropdown.Item>
-                    <Dropdown.Item onClick={filter.watched !== 'Watched' ? () => {setFilter({public: filter.public, watched: 'Watched'})} : null}>Watched</Dropdown.Item>
-                    <Dropdown.Item onClick={filter.watched !== 'Still to watch' ? () => {setFilter({public: filter.public, watched: 'Still to watch'})} : null}>Still to Watch</Dropdown.Item>  
-                </DropdownButton>
-            </div>
-            </Col>
+                    <DropdownButton 
+                        as={ButtonGroup} 
+                        size="md"
+                        className={`${appStyles.verticalMargin}`} 
+                        variant='outline-secondary' 
+                        title={
+                            <>
+                                <i className="fa-solid fa-filter"></i> {filter.watched}
+                            </>
+                        }
+                    >
+                        <Dropdown.Item onClick={filter.watched !== 'All' ? () => {setFilter({public: filter.public, watched: 'All'})} : null}>All</Dropdown.Item>
+                        <Dropdown.Item onClick={filter.watched !== 'Watched' ? () => {setFilter({public: filter.public, watched: 'Watched'})} : null}>Watched</Dropdown.Item>
+                        <Dropdown.Item onClick={filter.watched !== 'Still to watch' ? () => {setFilter({public: filter.public, watched: 'Still to watch'})} : null}>Still to Watch</Dropdown.Item>  
+                    </DropdownButton>
+                </ButtonGroup>            
+            </Col>  
         </Row>
-
-        </>
+        </Container>
         )
 }
 
