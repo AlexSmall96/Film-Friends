eb63abe9b1fa27ee9431f33eb8cfc9fe1bde48f8
Alex Small <alex.small739@gmail.com>
2024-12-10 12:34:51 +0000

    Refactor pages array to include unique keys

--- src/components/ResultsPagination.jsx
+++ src/components/ResultsPagination.jsx
@@ -10,34 +10,34 @@ const ResultsPagination = ({currentPage, setCurrentPage, finalPage}) => {
     const [pages, setPages] = useState([])
     // Use current page and final page to determine pages array
     useEffect(() => {
-        const show = width >= 576? 10 : 6
+        const show = width >= 475? 10 : width >= 387? 8 : 6
         const items = []
         if (finalPage <= show) {
             for (let i=1;i<=finalPage;i++){
-                items.push(i)
+                items.push({key: i, display: i})
             }
         } else {
             if (currentPage <= 3) {
                 for (let i=1;i<=show-2;i++){
-                    items.push(i)
+                    items.push({key: i, display: i})
                 }
-                items.push('...')
-                items.push(finalPage)
+                items.push({key: finalPage - 1, display: '...'})
+                items.push({key: finalPage, display: finalPage})
             } else if (currentPage <= finalPage - show + 2){
-                items.push(1)
-                items.push('...')
+                items.push({key: 1, display: 1})
+                items.push({key: 2, display: '...'})
                 for (let i=currentPage;i<=currentPage + show - 5;i++){
-                    items.push(i)
+                    items.push({key: i, display: i})
                 }
-                items.push('...')
-                items.push(finalPage)
+                items.push({key: finalPage - 1, display: '...'})
+                items.push({key: finalPage, display: finalPage})
             } else {
-                items.push(1)
-                items.push('...')
+                items.push({key:1, display: 1})
+                items.push({key:2, display: '...'})
                 for (let i=finalPage-show + 3;i<=finalPage-1;i++){
-                    items.push(i)
+                    items.push({key: i, display: i})
                 }
-                items.push(finalPage)
+                items.push({key: finalPage, display: finalPage})
             }
         }
         setPages(items)
@@ -51,7 +51,7 @@ const ResultsPagination = ({currentPage, setCurrentPage, finalPage}) => {
             :''
             }
                 {pages.map(page => 
-                    <Button key={page} className={appStyles.noUnderline} variant={page === currentPage? 'secondary': 'link'} onClick={() => setCurrentPage(page)}>{page}</Button>
+                    <Button key={page.key} disabled={page.display === '...'} className={appStyles.noUnderline} variant={page.display === currentPage? 'secondary': 'link'} onClick={() => setCurrentPage(page.display)}>{page.display}</Button>
                 )}
             {currentPage !== finalPage?
                 <Button size='sm' disabled={currentPage === finalPage} onClick={() => setCurrentPage(currentPage + 1)} variant='link'><i className="fa-solid fa-chevron-right"></i></Button>  
