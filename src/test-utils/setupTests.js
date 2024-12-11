539e2f230f6866dcf488bb2be24f3d2c241dd277
Alex Small <alex.small739@gmail.com>
2024-12-11 15:39:49 +0000

    Move handlers and server into test utils folder

--- src/test-utils/setupTests.js
+++ src/test-utils/setupTests.js
@@ -1,5 +1,5 @@
-import { server } from '../mocks/server'
-import { handlers } from '../mocks/handlers'
+import { server } from './mocks/server'
+import { handlers } from './mocks/handlers'
 import { afterEach, beforeAll, afterAll, beforeEach } from 'vitest';
 import { cleanup  } from '@testing-library/react';
 
