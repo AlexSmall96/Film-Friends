8242d84143205fbdab8f7bcd6ee57a1e0b625f9f
Alex Small <alex.small739@gmail.com>
2025-03-07 16:54:38 +0000

    Update context for new component structure

--- src/contexts/CurrentFilmContext.jsx
+++ src/contexts/CurrentFilmContext.jsx
@@ -22,57 +22,14 @@ export const CurrentFilmProvider = ({ children }) => {
     const [viewingData, setViewingData] = useState({watched: false, userRating: ''})
     const [omdbData, setOmdbData] = useState({})
 
-    // Current user value, used for the token in updatedViewingData function below
-    const {currentUser} = useCurrentUser()
-    
-    // Updated boolean variable, to re-trigger useEffect in films page
-    const [updated, setUpdated] = useState(false)
-
-    // Updates a users rating, watched value, or public / private marking
-    const updateViewingData = async (event, value, publicFilm) => {
-        let reqObj, stateObj
-        if (event) {
-            reqObj = {watched: event.target.checked}
-            stateObj = {watched: event.target.checked, userRating: viewingData.userRating, public: viewingData.public}
-        } else if (value) {
-            reqObj = {userRating: value}
-            stateObj = {watched: viewingData.watched, userRating: value, public: viewingData.public}
-        } else {
-            reqObj = {public: publicFilm}
-            stateObj = {watched: viewingData.watched, userRating: viewingData.userRating, public: publicFilm}
-        }
-        try {
-            await axiosReq.patch(`/films/${currentFilmIds.database}`, reqObj, {headers: {'Authorization': `Bearer ${currentUser.token}`}} )
-            setViewingData(stateObj)
-        } catch(err) {
-            console.log(err)
-        }
-    }
-
-    // Saves a film to users watchlist, can be called via the buttons for each film result
-    const saveFilm = async (Title, imdbID, Poster, Year, Type, publicFilm) => {
-        try {
-            await axiosReq.post('/films', {Title, imdbID, Poster, Year, Type, public: publicFilm}, {
-                headers: {'Authorization': `Bearer ${currentUser.token}`}
-            })
-            setUpdated(!updated)
-        } catch(err){
-            console.log(err)
-        }
-    }
-
     return (
         <CurrentFilmContext.Provider value={{
             currentFilmIds,
-            setCurrentFilmIds, 
-            viewingData, 
+            setCurrentFilmIds,
+            viewingData,
             setViewingData,
             omdbData, 
-            setOmdbData,
-            updateViewingData,
-            saveFilm,
-            updated,
-            setUpdated
+            setOmdbData
         }}>
             {children}
         </CurrentFilmContext.Provider>
