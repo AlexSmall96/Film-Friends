6a9666a1adf6327e6a0eb43d65fd78433e9d8cd7
Alex Small <alex.small739@gmail.com>
2025-03-15 20:33:21 +0000

    Use DEV flag to set baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
+const DEV = process.env.DEV
 
 // Set base url
-// axios.defaults.baseURL = 'http://localhost:3001/data';
-axios.defaults.baseURL = 'https://film-friends.onrender.com/data';
+axios.defaults.baseURL = DEV ? 'http://localhost:3001/data' : 'https://film-friends.onrender.com/data'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = false;
 
