ab3489aba251c51109b282704bae92bd1de5c8d7
Alex Small <alex.small739@gmail.com>
2025-03-19 16:01:29 +0000

    Change local storage check to currentUser check

--- src/hooks/useRedirect.jsx
+++ src/hooks/useRedirect.jsx
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
 import { useHistory} from 'react-router';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 export const useRedirect = () => {
-  
-const history = useHistory()
-  useEffect(() => {
-    const checkUser = () => {
-        if (!localStorage.getItem('storedUser')){
+    const history = useHistory()
+  	const {currentUser} = useCurrentUser()
+
+    useEffect(() => {
+        const redirect = async () => {
             history.push('/')
         }
-    }
-    checkUser();
-  }, [history]);
+        if (!currentUser) {
+            redirect()
+        }
+    }, [currentUser])
 };
