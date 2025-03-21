e506a8e330e4edec2ba4ec0866e7438fde61ad53
Alex Small <alex.small739@gmail.com>
2025-03-21 12:58:54 +0000

    Change baseUrl back to https://film-friends.onrender.com/data

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set base url
-axios.defaults.baseURL =  'https://film-friends-api.onrender.com/data'
+axios.defaults.baseURL =  'https://film-friends.onrender.com/data'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = false;
 
