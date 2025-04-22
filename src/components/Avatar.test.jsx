6248c4a17b16b51ac86e5ac18bab9374d2130b00
Alex Small <alex.small739@gmail.com>
2025-04-22 15:26:25 +0100

    Add Avatar test file

---
+++ src/components/Avatar.test.jsx
@@ -0,0 +1,70 @@
+/**
+ * @vitest-environment jsdom
+ */
+import Avatar from './Avatar'
+import React from 'react';
+import '@testing-library/jest-dom/vitest';
+import { screen, render } from '@testing-library/react';
+import { test, expect} from 'vitest';
+import setupTests from '../test-utils/setupTests';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
+setupTests()
+
+// Render component with CurrentUserContext
+const renderWithContext = (component, currentUser) => {
+    return(
+        render(
+            <CurrentUserContext.Provider value={{currentUser}}>
+                {component}
+            </CurrentUserContext.Provider>
+        )
+    )
+}
+
+// Define currentUser data
+const currentUserImage = 'https://res.cloudinary.com/dojzptdbc/image/upload/v1744368051/defaultProfile_hizptb.png'
+const currentUser = {user: {username: 'user1', _id: 'user1id', image: currentUserImage}}
+
+test('When rendered with all props, the image should have correct height, width and source.', () => {
+    const src = 'https://res.cloudinary.com/dojzptdbc/image/upload/v1744633674/defaultProfile_fjp9f4.png'
+    // Render component
+    renderWithContext(<Avatar src={src} height={100} />, null)
+    // Find image
+    const image = screen.getByRole('img', {name: 'avatar'})
+    expect(image).toBeInTheDocument()
+    // Image should have correct src, height and width
+    expect(image.src).toBe('https://res.cloudinary.com/dojzptdbc/image/upload/v1744633674/defaultProfile_fjp9f4.png')
+    expect(image.height).toBe(100)
+    expect(image.width).toBe(100)
+})
+
+test('When rendered with no props, the image should have default source and height.', () => {
+    // Render component
+    renderWithContext(<Avatar />, currentUser)
+    // Find image
+    const image = screen.getByRole('img', {name: 'avatar'})
+    expect(image).toBeInTheDocument()
+    // Image should have default src of current user image, and default height and width.
+    expect(image.src).toBe('https://res.cloudinary.com/dojzptdbc/image/upload/v1744368051/defaultProfile_hizptb.png')
+    expect(image.height).toBe(45)
+    expect(image.width).toBe(45)
+})
+
+test('When rendered with square prop as true, the image should have class name squareAvatar and not avatar.', () => {
+    // Render component with square as true
+    renderWithContext(<Avatar square />, currentUser)
+    const image = screen.getByRole('img', {name: 'avatar'})
+    expect(image).toBeInTheDocument()
+    expect(image).not.toHaveClass('_avatar_a0aacb')
+    expect(image).toHaveClass('_squareAvatar_a0aacb')
+})
+
+test('When rendered with square prop as false, the image should have class name avatar and not squareAvatar', () => {
+    // Render component with square as false
+    renderWithContext(<Avatar />, currentUser)
+    const image = screen.getByRole('img', {name: 'avatar'})
+    expect(image).toBeInTheDocument()
+    expect(image).not.toHaveClass('_squareAvatar_a0aacb')
+    expect(image).toHaveClass('_avatar_a0aacb')
+})
